090ffd163dceb103b651dc31398c754514485be9
Arthur Blois <arthur.blois@etu.uca.fr>
2025-07-01 10:04:53 +0200

    rewrite the code to make it reusable

--- 30_js/burgerMenu.js
+++ 30_js/burgerMenu.js
@@ -1,9 +1,19 @@
-window.onload = () => {
-    const burger = document.getElementById('burger');
-    const navLinks = document.getElementById('nav-links');
+export function setupBurgerMenu() {
+  const burger = document.getElementById('burger');
+  const navLinks = document.getElementById('nav-links');
 
-    burger?.addEventListener('click', () => {
-        burger.classList.toggle('active');
-        navLinks?.classList.toggle('active');
+  if (burger && navLinks) {
+    burger.addEventListener('click', () => {
+      navLinks.classList.toggle('active');
+      burger.classList.toggle('active');
     });
-};
+
+    document.addEventListener('click', (event) => {
+      const clickInside = burger.contains(event.target) || navLinks.contains(event.target);
+      if (!clickInside && navLinks.classList.contains('active')) {
+        navLinks.classList.remove('active');
+        burger.classList.remove('active');
+      }
+    });
+  }
+}
